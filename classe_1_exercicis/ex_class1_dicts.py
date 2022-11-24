f461533a29bdc7aa6965f3b18eddf89ace444162
Xavi <xavierfdezlflores@gmail.com>
2022-11-24 13:16:26 +0100

    update

---
+++ classe_1_exercicis/ex_class1_dicts.py
@@ -0,0 +1,83 @@
+""" Introducció als diccionaris
+
+Els diccionaris de python són estructures de dades ordenades que s'utilitzen per a 
+emmagatzemar informació en parells de key:value. Cada element que hi guardem té associada
+una clau que es fa servir per accedir al valor. La seva sintaxi és diferent a la de les
+llistes per tal de poder diferenciar-les. Cada parell de clau:valor és únic!
+
+i.e: dict_prova = {
+    "marca": "Renault",
+    "model": "Clio",
+     "any": 1996,
+     "colors":['blau','vermell','blanc']
+    }
+    
+    dict_prova["marca"] #retorna el valor "Renault"
+    dict_prova["colors"][1] #retorna "vermell"
+    
+Els mètodes associats als diccionaris els podeu trobar a:
+    https://www.w3schools.com/python/python_ref_dictionary.asp
+    
+Els diccionaris també són iterables. Es poden iterar per claus, valors o les dues coses.
+Farem servir els mètodes values(), keys() i items()
+
+i.e: 
+
+    for i in dict_prova:
+        print(dict_prova[i]) #retorna els valors del diccionari
+        
+    for i in dict_prova.values():
+        print(i) #fa el mateix que l'anterior. Retorna els valors del diccionari
+        
+    for i in dict_prova.keys():
+        print(i) #retorna les claus del diccionari
+        
+    for i in dict_prova.items():
+        print(i) #retorna un tuple dels parells key:value
+        
+    for i, j in dict_prova.items():
+        print(i, j) #retorna claus i valors per separat
+        
+Els diccionaris, igual que les llistes, es poden "anidar" (nested dictionaries, nested lists)
+Això significa que es poden posar diccionaris dintre de diccionaris.
+
+i.e: dict_nested = {
+    "animal_1":{"nom":"Altramuz",
+                "especie":"Gos",
+                "good_boy":True},
+    "animal_2":{"nom":"Petunia",
+                "especie":"Oca",
+                "good_boy":False},
+    "animal_3":{"nom": "Alpha",
+                "especie":"gos",
+                "color":['negre','taronja','blanc'],
+                "good_boy":True}
+}
+
+Accedim als seus valors de la següent manera:
+
+    dict_nested["animal_1"]["nom"] #retorna "Altramuz"
+    dict_nested["animal_2"]["good_boy"] #retorna False
+    dict_nested["animal_3"]["color"][0] #retorna "negre"
+     
+Els diccionaris són especialment útils quan treballem amb variables complexes i ens
+ajuden a que el nostre codi sigui més llegible.
+"""
+
+# Exercici 1
+
+""" Fes un diccionari que sigui la fitxa d'un alumne de la uni: Nom, NIU, edat,
+email, direcció i telefon.
+"""
+
+# Exercici 2
+
+""" Amb el diccionari de l'exercici 1, canvia el valor del número de telèfon i el NIU
+de l'estudiant fent servir un dels mètodes propis dels diccionaris.
+"""
+
+# Exercici 3
+
+""" Fes un diccionari anidat que contingui les fitxes de tres alumnes amb les mateixes
+claus que a l'exercici 1.
+"""
---
+++ classe_1_exercicis/ex_class1_llistes.py
@@ -0,0 +1,150 @@
+"""Introducció a llistes
+
+Les llistes són una estructura de dades molt útil per a emmagatzemar informació
+i poder utilitzar-la i accedir a ella de manera controlada. Són mutables
+(els seus elements poden canviar), permeten emmagatzemar tot tipus d'elements
+i són iterables (es pot fer un loop sobre els seus elements). A més, admeten
+operacions amb operadors algebraics com (*) (la multiplicació, que sobre llistes
+actua com a concatenador).
+
+i.e: n = 3
+
+    llista = [True]* n 
+    print(llista)
+    
+    #resultat: [True, True, True]
+    
+Les llistes també estan indexades, el que significa que cada element de la llista té
+associada una posició. En python, les llistes comencen en 0, i per accedir a elements 
+concrets fem servir la següent sintaxi:
+
+i.e: lst = ['primer', 'segon', 'tercer']
+    print(lst[0])  #resultat: 'primer'
+    print(lst[1])  #resultat: 'segon'
+    print(lst[2])  #resultat: 'tercer'
+    
+    #si li demanem un índex que no existeix:
+    
+    print(lst[3])  #resultat: "IndexError: list index out of range"
+
+
+Per iniciar-les es pot fer una "list comprehension", que té una sintaxi més
+intuitiva o bé anar afegint valors des d'un bucle for o while. També es poden fer
+llistes buides i anar-les omplint amb els valors que vulguem.
+
+i.e: empty_list = [] #llista buida
+
+    list_2 = [x for x in range(1, 42)]
+    
+    list_2 = []
+    for x in range(1, 42):
+        list_2.append(x)
+
+
+Les llistes també tenen tot un seguit de funcions bàsiques que se'ls poden aplicar
+pel sol fet de ser llistes. Aquestes funcions (també anomenades mètodes) les trobareu a 
+
+    https://www.w3schools.com/python/python_ref_list.asp
+
+Les aplicarem mitjançant l'operador punt (.) sobre una llista definida.
+
+i.e: llista_2 = []
+
+    print(llista_2)  #resultat: []
+    
+    llista_2.append(4)
+    llista_2.append('hola')
+    
+    print(llista_2) #resultat [4, 'hola']
+    
+    llista_2.pop(0)
+    
+    print(llista_2) #resultat ['hola']
+    
+Les llistes treballent molt bé juntament amb variables de tipus comptador que guardin
+un valor que va canviant en el temps. 
+
+i.e: counter = 0 #inicialitzem un comptador
+    list_3 = [1,2,3,4,5]
+    
+    for item in list_3:
+        if not item % 2:   #el % és l'operació mòdul. Aquesta línea dona els nombres parells
+            counter += item
+            
+    print(counter)
+
+"""
+
+# Exercici 1
+
+"""Fes una llista que contingui números del 1 al 25. Pots fer-ho amb la funció range
+dels loops for i la funció append(), amb una list comprehension o com tu vulguis.
+"""
+
+list_ex1 = [x for x in range(1, 26)]
+
+list_ex1_prima = []
+for x in range(1, 26):
+    list_ex1_prima.append(x)
+
+
+# Exercici 2
+
+"""Fes una llista amb N elements (els que vulguis) i fes servir un loop for per iterar sobre tots ells.
+Per cada element, fes que s'imprimeixi per pantalla amb la funció print(). Per iterar sobre
+una llista amb un for loop, fem servir la sintaxi "for item in list:"
+"""
+
+list_2 = [1, 2, 3]
+
+for item in list_2:
+    print(item)
+
+
+# Exercici 3
+
+"""Fes una funció o programa que donada una llista et sumi tots els seus elements. Pots
+fer servir un for loop i l'ajut d'una variable "counter" que vagi emmagatzemant el valor
+final de la suma.
+"""
+
+list_3 = [x for x in range(9)]
+counter = 0
+for item in list_3:
+    counter += item
+
+
+# Exercici 4
+
+"""Fes una llista que retorni el nombre més gran d'una llista de nombres enters
+"""
+
+# Exercici 5
+
+"""Fes una llista de strings. 
+i.e: lst_str = ["plusquamperfet", "elefant", "plenitud", "isomer"]
+i fes que l'ordinador imprimeixi per pantalla la string més llarga.
+
+PISTA: el mètode (o funció) len() ens ajuda a saber la longitud d'una llista.
+Curiosament, les strings a python són iterables, lo qual les converteix quasi bé en llistes
+a efectes pràctics. 
+
+i.e: print(len(lst_str[2])) #impimeix el número de caràcters (longitud) de la string a la posició 2
+    #resultat: 8 (plenitud té 8 lletres)
+"""
+# Exercici 6
+
+"""Fes una funció que, donada una llista, et vagi el·liminant tots els seus elements a 
+mesura que va iterant sobre ella.
+
+PISTA: Mira què fa la funció pop() que actua sobre llistes. 
+"""
+
+# Exercici 7
+
+"""Fes una funció que iteri sobre una llista de nombres enters i imprimeixi "True" 
+si la llista conté dos o més elements iguals.
+
+i.e: lst = [1,1,3,3,4,6,7] #True
+    lst2 = [1,2,3,4,5,6] #False
+"""
