b8e230bfc3d163a83df99960d3af8c91c72fdcca
xavioist <xavierfdezlflores@gmail.com>
2022-12-02 16:52:03 +0100

    add basic exercises

---
+++ classe_1_exercicis/ex_classe1_primers_passos.py
@@ -0,0 +1,60 @@
+# Exercici 1
+
+""" Donades les variables x i y (de les quals nosaltres tenim complert control), fes un programa que compari aquestes dues variables i:
+- imprimeixi per pantalla la que sigui més gran.
+- li sumi 5 a la variable més petita i la imprimeixi per pantalla
+- imprimeixi el missatge "son iguals" en cas de que ho siguin.
+"""
+
+
+# Exercici 2
+
+""" Fes un programa que, quan li donis un nombre parell et retorni per pantalla el missatge "el nombre X és parell". I si és imparell, que retorni
+per pantalla "el nombre X no és parell" on X és el nombre en qüestió.
+
+PISTA: la funció print() pot imprimir variables entre missatges de text. i.e:
+
+x = 42
+
+print("el número ", x, " és parell") >>> 'el número 42 és parell'
+"""
+
+
+# Exercici 3
+
+""" Inicialitza una llista que contingui 30 valors fent servir un bucle for i la funció range()
+"""
+
+# Exercici 4
+
+""" Donada la llista ex1_list = [1,1,2,3,5,8,13,21] fes servir un bucle for per a obtenir el valor de la suma de tots els seus components
+"""
+
+# Exercici 5
+
+""" Una bona manera de fer servir el "while" és per a comprovar quan una variable haurà prés un valor determinat. Amb l'ajuda d'una variable extra
+que anomenarem "counter" i que començarà a 0 (counter = 0), fes un bucle while que s'executi fins que coutner sigui més gran o igual a 99, 
+i que quan ho sigui imprimeixi per pantalla un missatge (et pots ajudar d'un bloc "if").
+"""
+
+# Exercici 6
+
+""" Fes una funció que s'anomeni suma i accepti dos valors:
+
+suma (x, y)
+
+i que retorni la suma de les dues variables x i y imprimint per pantalla el resultat. 
+"""
+
+# Exercici 7
+
+""" Donada la llista ex7_list = [1,3,5,7,9] i la llista even_lst = [2,4,6,8,10], fes un programa que a partir de les dues et generi una llista nova
+amb tots els valors de la primera i la segona llista. i.e: el resultat final ha de ser [1,2,3,4,5,6,7,8,9,10]
+"""
+
+
+# Exercici 8
+
+""" Fent servir bucles for, condicionals i tantes variables com necessitis, fes una funció que donada la llista ex8_list = [3,6,2,5,7,4,1] et retorni
+una llista ordenada.
+"""
