70e3b3ec2eea592102cca2687c8bc14d9a0bbed1
Xavi <xavierfdezlflores@gmail.com>
2023-01-03 14:26:52 +0100

    classe 3 ampliacio

--- classe_3_exercicis/ex_classes1.py
+++ classe_3_exercicis/ex_classes1.py
@@ -1,14 +1,35 @@
+"""Exemple de classes
+"""
+
+
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    def change_name(self, new_name):
+        self.name = new_name
+
+
+class Student(Person):
+    def __init__(self, name, age, niu, grade_list):
+        super().__init__(name, age)
+        self.niu = niu
+        self.grade_list = grade_list
+
+    def average_grade(self):
+        from functools import reduce
+
+        sum = reduce(lambda a, b: a + b, self.grade_list)
+        return sum / len(self.grade_list)
+
+
+person_3 = Student("Kyle", 22, 1329983, [5, 6, 7])
+person_3.change_name("Johnny")
+
+print(person_3.average_grade())  # 6.0
 
 persona_1 = Person("Carles", 23)
 persona_2 = Person("Sílvia", 24)
 
-print(persona_1.name)
-
-persona_1.name = "Mohammed"
-
-print(persona_1.name)
+persona_1.change_name("Mohammed")
---
+++ classe_3_exercicis/ex_classes2.py
@@ -0,0 +1,30 @@
+# Exercici 1
+""" Fes una classe que s'anomeni Atom i tingui les següents propietats:
+- Pes molecular (float)
+- Simbol de l'element (string)
+- Nombre atòmic (int)
+- Classificació a la taula periòdica (string) (gas noble, lantànid, alcalí, etc...)
+- Coordenades a l'espai (list[float]) [poseu unes coordenades qualsevols]
+
+I que inclogui els mètodes següents:
+
+- Un mètode que es digui compare_atoms() i et digui quin dels dos àtoms té
+un pes molecular major
+
+- Un mètode que retorni True si l'àtom és un gas noble i False si no ho és
+
+- Un mètode que et retorni la distància entre dos àtoms
+"""
+
+# Exercici 2
+""" Prenent de referència la classe Atom que s'ha fet, afegeix-hi un mètode que
+agafi dos àtoms i, segons la seva distància, et digui si poden estar enllaçats o no
+
+PISTA: Pots generar una taula amb distàncies d'enllaç típiques entre atoms.
+Pren de referència la imatge taula_bond_distance.png
+
+Un cop s'ha calculat la distància entre àtoms, ha de dir per pantalla si
+poden o no estar enllaçats.
+
+(si dist_atoms < dist_enllaç -> possible)
+"""
