d7ea6adaa539a349e823ca0de8dc53a8d05139ee
Xavi <xavierfdezlflores@gmail.com>
2023-04-05 12:15:40 +0200

    tidying up

--- classe_3_exercicis/ex_resolt_classes.py
+++ classe_3_exercicis/ex_resolt_classes.py
@@ -3,6 +3,18 @@
 
 
 class Person:
+    """Class Person
+
+    atributs:
+    name = (string)
+    age = (int)
+
+    mètodes:
+       change_name(new_name)::str -> str::
+            canvia el nom d'un objecte de la classe
+            Person
+    """
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -12,12 +24,28 @@ class Person:
 
 
 class Student(Person):
+    """Classe Student, hereda name i age de la classe Person
+
+    Un objecte de Student forma part de Person, però un objecte de Person
+    no necessàriament és un Student.
+
+    atributs:
+    niu = (int)
+    grade_list = (list[float])
+
+    mètodes:
+        average_grade():: Class_Student_object -> float::
+            calcula la nota mitjana d'un Student i retorna un float
+    """
+
     def __init__(self, name, age, niu, grade_list):
+        # Aquesta línea ens dóna l'herència. Ara Student també té name i age
         super().__init__(name, age)
         self.niu = niu
         self.grade_list = grade_list
 
     def average_grade(self):
+        # Prova a reescriure una funció que calculi la nota  mitjana d'un Student
         from functools import reduce
 
         sum = reduce(lambda a, b: a + b, self.grade_list)
