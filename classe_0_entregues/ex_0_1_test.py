fc44db1a42198c830e1394caf42215efb4faed38
xavioist <xavierfdezlflores@gmail.com>
2023-04-11 16:20:43 +0200

    retoc entrega 1

--- classe_0_entregues/ex_0_1_test.py
+++ classe_0_entregues/ex_0_1_test.py
@@ -1,19 +1,19 @@
 """Ex 0.1
 Defineix dues variables:
 
-var1 = 66
+var1 = "66"
 
 var2 = "foo bar baz"
 
 
 Fes una funció generator(var1, var2) que, donades var1 i var2, sigui capaç de generar var3
 
-var3 = [66, "foo", 132, "ar"]
+var3 = ["66", "foo bar baz", "bar"]
 
-PISTA: Pots fer servir el slicing de llistes i strings i el mètode append() de les llistes 
+PISTA: Pots fer servir mètode append() de les llistes i el slicing de llistes de python: https://www.w3schools.com/python/python_strings_slicing.asp
 """
 
-var1 = 66
+var1 = "66"
 
 var2 = "foo bar baz"
 
@@ -21,9 +21,10 @@ var2 = "foo bar baz"
 def generator(var1, var2):
     out = []
     out.append(var1)
-    out.append(var2[0:3])
-    out.append(2 * var1)
-    out.append(var2[5:7])
+    out.append(var2)
+
+    tmp = var2[3:7]
+    out.append(tmp)
     return out
 
 
