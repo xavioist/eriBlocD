e846c471d1dd82f73a1963a330b7cb42f702f562
xavioist <xavierfdezlflores@gmail.com>
2023-04-24 15:28:49 +0200

    fixed errata

--- classe_0_entregues/ex_0_2_test.py
+++ classe_0_entregues/ex_0_2_test.py
@@ -4,7 +4,7 @@ Fes una funció check(numero) que, donat qualsevol nombre enter positiu,
 digui si és parell i divisible per 7. Si no ho és, que imprimeixi per pantalla si és
 parell, divisible per 7 o cap de les opcions anteriors.
 
-Fes una altra funció resta(numero) que et digui quants cops se li pot restar 3 abans de que el nombre
+Fes una altra funció resta(numero) que et digui quants cops se li pot restar 3 a un nombre enter positiu abans de que el nombre
 esdevingui negatiu.
 """
 
